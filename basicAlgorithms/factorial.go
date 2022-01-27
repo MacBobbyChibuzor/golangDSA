a1f8d14ae54c6d894f3379f264630c3fecd4bb58
MacBobby Chibuzor <97325908+MacBobbyChibuzor@users.noreply.github.com>
2022-01-28 00:31:14 +0100

    recursion application

---
+++ basicAlgorithms/factorial.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func factorial(n int) int {
+    if num == 0 {
+	        return 1
+			    } else {
+				        return num * factorial(num - 1)
+			    }
+							}
+
+							func main() {
+							    factorial(5)
+								}
